a179d5c441c2a9ccf8b42ad9259e7db6e14be91c
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-07-28 17:52:43 +0330

    feat : app download section

--- app/components/sections/Testimonials.jsx
+++ app/components/sections/Testimonials.jsx
@@ -108,7 +108,7 @@ export default function Testimonials() {
   };
 
   return (
-    <section className="py-32 bg-gradient-to-br from-gray-50 to-blue-50 ">
+    <section className="py-32 bg-white ">
       <motion.div
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
         variants={containerVariants}
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/sections/Navbar";
 import Hero from "./components/sections/Hero";
-import ProblemSolutionSection from "./components/sections/ProblemSolutionSection";
-import AnimationWrapper from "./components/sections/AnimationWrapper";
 import ZoomEffect from "./components/sections/ZoomEffect";
 import Testimonials from "./components/sections/Testimonials";
 import FAQSection from "./components/sections/faq";
 import Footer from "./components/sections/Footer";
 import CinematicParallaxStory from "./components/sections/CinematicParallaxStory";
+import DownloadCta from "./components/sections/DownloadCta";
+
 export default function Home() {
   return (
     <>
@@ -17,11 +17,10 @@ export default function Home() {
       <ZoomEffect />
       <CinematicParallaxStory />
 
-      <div className="relative z-50">
-        <Testimonials />
-        <FAQSection />
-        <Footer />
-      </div>
+      <Testimonials />
+      <DownloadCta />
+      <FAQSection />
+      <Footer />
     </>
   );
 }
