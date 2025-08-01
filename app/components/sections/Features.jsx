86264b09c126ededd9113e39e96ec0b9a70f3edd
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-08-01 19:01:31 +0330

    refactor: Extract hardcoded texts and constants to dedicated consts file
    This commit refactors various sections by moving hardcoded strings and static values into a centralized constants file. This improves code organization, readability, and maintainability by reducing clutter within component files and promoting reusability of text content.

--- app/components/sections/Features.jsx
+++ app/components/sections/Features.jsx
@@ -3,75 +3,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import icon2 from "@/public/icons/golds.png";
-import icon3 from "@/public/icons/reason.png";
-import icon4 from "@/public/icons/analysis.png";
-import icon5 from "@/public/icons/challanges.png";
-import icon6 from "@/public/icons/cart.png";
-import icon7 from "@/public/icons/check-list.png";
-import icon8 from "@/public/icons/search.png";
-import icon9 from "@/public/icons/chart2.png";
-import icon10 from "@/public/icons/reminder.png";
-import icon11 from "@/public/icons/category.png";
-import icon12 from "@/public/icons/management.png";
-
-const features = [
-  {
-    id: 1,
-    title: "ثبت خودکار تراکنش",
-    icon: icon7,
-  },
-  {
-    id: 2,
-    title: "دسته‌بندی تراکنش‌ها",
-    icon: icon11,
-  },
-  {
-    id: 3,
-    title: "نمودارهای تحلیلی مالی",
-    icon: icon9,
-  },
-  {
-    id: 4,
-    title: "علت‌یابی تراکنش",
-    icon: icon3,
-  },
-  {
-    id: 5,
-    title: "مدیریت حساب بانکی",
-    icon: icon12,
-  },
-  {
-    id: 6,
-    title: "فیلتر و جستجو",
-    icon: icon8,
-  },
-  {
-    id: 7,
-    title: "مدیریت لیست خرید",
-    icon: icon6,
-  },
-  {
-    id: 8,
-    title: "یادآوری تراکنش‌ها",
-    icon: icon10,
-  },
-  {
-    id: 9,
-    title: "چالش‌های مالی",
-    icon: icon5,
-  },
-  {
-    id: 10,
-    title: "تحلیل هوشمند مالی",
-    icon: icon4,
-  },
-  {
-    id: 11,
-    title: "قیمت طلا و ارز",
-    icon: icon2,
-  },
-];
+import features from "@/app/constants/features";
 
 // Container Animation Variants
 const containerVariants = {
